923999af550902d4b91bc7359781b42e60a40371
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2023-03-12 18:36:35 +0900

    fix : module import 문제 해결, random photo 완성

--- JS/photo.js
+++ JS/photo.js
@@ -4,6 +4,6 @@ const photoList = ["1.png", "2.png", "3.png", "5.png", "6.png", "7.png", "4.jpeg
 
 const getPhoto = () => {
     const img = photoList[Math.floor(Math.random() * photoList.length)];
-    photo.style = `background-image : url(assets/${img})`;
+    photo.style = `background-image : url(assets/${img});\n background-color : white;`;
     photoList.splice(photoList.indexOf(img), 1);
 };
