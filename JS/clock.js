3b1e487b950c35a1dfdd73eadd9f35735290a5c8
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2023-03-12 17:55:29 +0900

    add : timer

---
+++ JS/clock.js
@@ -0,0 +1,41 @@
+import { getPhoto } from "./photo";
+
+const clock = document.querySelector(".clock");
+const btn = document.querySelector(".btn__start");
+let seconds = 30;
+let toggle = false;
+let done = true;
+
+const getSeconds = () => {
+    seconds--;
+    clock.innerText = `남은 시간 : ${seconds}초`;
+    if (seconds === 0) {
+        clearInterval(timer);
+        toggle = false;
+        done = true;
+    }
+};
+
+let timer = setInterval(getSeconds, 1000);
+clearInterval(timer);
+
+const onClick = () => {
+    if (done) {
+        getPhoto();
+    }
+    done = false;
+    if (seconds === 0) {
+        seconds = 30;
+    }
+    if (toggle) {
+        clearInterval(timer);
+        btn.innerText = "시작";
+        toggle = false;
+    } else {
+        timer = setInterval(getSeconds, 1000);
+        btn.innerText = "일시중지";
+        toggle = true;
+    }
+};
+
+btn.addEventListener("click", onClick);
---
+++ JS/photo.js
@@ -0,0 +1,4 @@
+const photo = document.querySelector(".photo");
+
+const photoList = [];
+export const getPhoto = () => {};
